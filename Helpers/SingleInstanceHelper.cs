3fbae5e367f18c351ec746aa14cbab2520356283
Mohamed Aymen <aymen.kernel@gmail.com>
2026-02-05 21:33:45 +0100

    add Licence texte

--- Helpers/SingleInstanceHelper.cs
+++ Helpers/SingleInstanceHelper.cs
@@ -1,3 +1,13 @@
+/*
+ * WinUninstallDoctor - Windows uninstaller cleanup and diagnostic tool
+ * Copyright (C) 2026 Mohamed Aymen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 using System.Threading;
 
 public static class SingleInstanceHelper
